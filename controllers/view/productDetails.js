f28d1277e2827e9b096225e069d2f7b86a7b55f7
azam sedarat <nahid.sedarat.j@gmail.com>
2022-10-24 18:16:47 +0330

    product details

--- controllers/view/productDetails.js
+++ controllers/view/productDetails.js
@@ -1,27 +1,23 @@
 const db = require("../../models");
-
+const {Op} = require("sequelize");
+const { sequelize } = require("../../models");
 const getProductDetails= async(req, res) => {
     try {
 
-        // const product= await db.Product.findOne({
-        //      where:{id:req.params.productId},
-        //      include:{
-        //         model: db.ProductFeature,
-        //         attributes: ['id','varientFeature','photo'],
-        //         include:{
-        //             model: db.Seller,
-        //             attributes: ['id','qualify','storeName'],
-        //           }
-        //       }
-        //     })
-        const category= await db.Category.findAll({
-            include: {
-              model: db.Category,
-              as: 'Parent'
-            }
-          });
-        res.send(category)
-        // res.render("productDetails", {product})
+        const product= await db.Product.findByPk(req.params.productId,{
+             include:{
+                model: db.ProductFeature,
+                attributes: ['id','varientFeature','photo'],
+                include:{
+                    model: db.Seller,
+                    attributes: ['id','qualify','storeName'],
+                  }
+              }
+            })
+            
+        const category = await db.Category.findByPk(product.CategoryId, {attributes :["name"]})
+        
+        res.render("productDetails", {product,category})
     } catch (error) {
         
         res.send(error.message)
--- models/category.js
+++ models/category.js
@@ -4,14 +4,23 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Category.hasMany(models.Product),
-      Category.belongsTo(Category, { foreignKey: 'parentId', as: 'Parent' }); 
+
+
+      // relate a category to its parent=
+      Category.belongsTo(Category, {
+        as: 'parent', 
+        foreignKey: 'parentId', 
+        targetKey: 'id',
+      });
+
+      
+      // relate parent to child categories
+      Category.hasMany(Category, {
+        as: 'subcategories',
+        foreignKey: 'parentId',
+      });
     }
   }
   Category.init({
