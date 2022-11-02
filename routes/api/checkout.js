49a827c485671d49ef8ac42c4f5b9057045ef976
azam sedarat <nahid.sedarat.j@gmail.com>
2022-11-02 18:23:02 +0330

    remove unuseable files

--- routes/api/checkout.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const  { checkoutController , checkoutVerifyController } = require('../../controllers');
-const router = express.Router()
-
-router.post("/buy", checkoutController.createTransaction)
-router.post("/callback", checkoutVerifyController.callback)
-
-
-module.exports = router;
