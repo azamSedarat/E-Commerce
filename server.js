bf3357edd187083b80ffee3308175335d56b0580
azam sedarat <nahid.sedarat.j@gmail.com>
2022-11-02 19:28:56 +0330

    add middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "pug")
 
