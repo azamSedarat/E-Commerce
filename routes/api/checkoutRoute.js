2169f1d6f6a8256d2e73d435aee2e8afcbf66874
azam sedarat <nahid.sedarat.j@gmail.com>
2022-11-02 18:25:41 +0330

    edit name

--- routes/api/checkoutRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const  { checkoutController } = require('../../controllers');
-const router = express.Router()
-
-router.post("/buy", checkoutController.createTransaction)
-router.post("/callback", checkoutController.verifyCallback)
-
-
-module.exports = router;
