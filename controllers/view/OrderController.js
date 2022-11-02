cd45eff2028f275ae2b010c583cf850cbaf98d96
azam sedarat <nahid.sedarat.j@gmail.com>
2022-11-02 15:43:53 +0330

    add controller

---
+++ controllers/view/OrderController.js
@@ -0,0 +1,15 @@
+const {orderService} =  require("../../services")
+
+export class OrderController {
+
+  async getOrders(req, res) {
+    try {
+        const orders = orderService.getAllOrders(req.customerId)
+            res.send(orders)      
+    } catch (error) {
+        return res.render("error", { errors: error.message })
+    }
+}
+}
+
+module.exports = new OrderController();
